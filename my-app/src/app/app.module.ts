bb7410ddf474a1cfde3aff3a569fc26c13a67de5
tniu <tniu@infomedia.com.au>
2019-06-14 22:42:08 +1000

    Initial

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -2,13 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AgGridModule } from 'ag-grid-angular';
+import {AgGridModule} from "ag-grid-angular/src/aggrid.module";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AgGridModule.withComponents([]),
+    AgGridModule
   ],
   providers: [],
   bootstrap: [AppComponent]
