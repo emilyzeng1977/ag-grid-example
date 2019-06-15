6b79630971e1df2ff18695be1732cf07f07e10d4
tniu <tniu@infomedia.com.au>
2019-06-15 11:56:11 +1000

    CustomRenderer

--- cellRendererFrameworkExample/src/app/app.component.ts
+++ cellRendererFrameworkExample/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { DatePipe, DecimalPipe } from "@angular/common";
+import {DemoAgRendererComponentComponent} from "./demo-ag-renderer-component/demo-ag-renderer-component.component";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'cellRendererFrameworkExample';
+  constructor(private dateFormatter: DatePipe,
+              private numberFormatter: DecimalPipe) {}
+  title = 'my-app';
+
+  columnDefs = [
+    {headerName: 'Make', field: 'make'},
+    {headerName: 'Model', field: 'model'},
+    {
+      headerName: 'Price',
+      field: 'price',
+      cellRendererFramework: DemoAgRendererComponentComponent,
+      cellRendererParams: {
+
+      }
+    },
+    {
+      headerName: 'Date',
+      field: 'date',
+      valueFormatter: (data) => this.dateFormatter.transform(data.value, 'shortDate')
+    }
+  ];
+
+  rowData = [
+    { make: 'Toyota', model: 'Celica', price: 35000, date: '2018-01-04'},
+    { make: 'Ford', model: 'Mondeo', price: 32000, date: '2019-06-14' },
+    { make: 'Porsche', model: 'Boxter', price: 72000, date: '2019-06-12' }
+  ]
 }
--- cellRendererFrameworkExample/src/app/app.module.ts
+++ cellRendererFrameworkExample/src/app/app.module.ts
@@ -1,16 +1,27 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {AgGridModule} from 'ag-grid-angular';
+import {DemoAgRendererComponentComponent} from './demo-ag-renderer-component/demo-ag-renderer-component.component';
+import {DatePipe, DecimalPipe} from "@angular/common";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DemoAgRendererComponentComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AgGridModule.withComponents([
+        DemoAgRendererComponentComponent
+      ]
+    )
   ],
-  providers: [],
+  providers: [
+    DatePipe,
+    DecimalPipe
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
